9322bc52ae82316f5a7bb6b441ba169d59007774
Justin Klaver <Klaverts@yahoo.com>
2022-11-17 13:01:50 -0500

    note js added to provide functionality

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
 const express = require('express');
 
+const PORT = process.env.PORT || 3000;
 const app = express();
+const apiRoutes = require('./routes/api-Routes');
+const htmlRoutes = require('./routes/html-Route');
+
+// Parse URL encoded & JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Host public folder
+app.use(express.static('public'));
+
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
